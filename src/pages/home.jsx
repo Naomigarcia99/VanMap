495bc286a15186a9d28863ffef9cc09cef802356
Naomi García Robles <naomigarciarobles21@gmail.com>
2025-01-13 02:18:29 +0100

    Home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,36 @@
-import React from "react";
-import Map from "../components/Map";
-import RouteForm from "../components/RouteForm";
+import { Link } from "react-router-dom";
+import Van from "../assets/images/van.mp4";
 
-function Home() {
+const Home = () => {
   return (
-    <div className="min-h-screen bg-pastelBeige flex flex-col items-center">
-      <header className="w-full bg-pastelBlue text-center py-4 shadow-md">
-        <h1 className="text-2xl font-bold">Planificador de Rutas</h1>
-      </header>
-      <main className="flex-1 w-full max-w-md p-4">
-        <RouteForm />
-        <Map />
-      </main>
+    <div className="relative w-full h-screen overflow-hidden">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover max-w-full max-h-screen"
+      >
+        <source src={Van} type="video/mp4" />
+        Tu navegador no soporta la reproducción de videos.
+      </video>
+
+      <div className="relative flex flex-col justify-center items-center h-full text-white bg-black bg-opacity-50">
+        <h1 className="text-4xl font-semibold text-center mb-6">
+          Bienvenidos a VanMap
+        </h1>
+        <p className="text-xl mb-4 text-center">
+          Tu app para planificar y guardar rutas para tus viajes en caravana.
+        </p>
+        <Link
+          to="/map"
+          className="bg-blue-500 text-white p-3 rounded-full shadow-md text-center w-auto"
+        >
+          Empezar
+        </Link>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
