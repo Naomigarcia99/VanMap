c2b8fbf98b414d33eae0e0990fc95cb9891df9f2
Naomi García Robles <naomigarciarobles21@gmail.com>
2025-01-20 11:11:19 +0100

    Map configuration

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,36 +1,17 @@
 import React, { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { useRouteContext } from "../context/RouteContext";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
 function Map() {
+  const { initializeMap } = useRouteContext();
   const mapContainerRef = useRef(null);
-  const map = useRef(null);
 
   useEffect(() => {
-    if (map.current) return;
-
-    map.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [-3.70379, 40.41678], // Madrid como punto inicial
-      zoom: 10,
-    });
-
-    //return () => map.remove();
-    //map.current.addControl(new mapboxgl.NavigationControl());
+    initializeMap(mapContainerRef.current);
   }, []);
-
-  /*useEffect(() => {
-    if (!map.current) return; // Verifica que el mapa esté inicializado
-
-    map.current.on("move", () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  }, []);*/
 
   return (
     <div
