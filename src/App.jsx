06f651d7be7d728944cdf55fe2850046292b8650
Naomi García Robles <naomigarciarobles21@gmail.com>
2025-01-20 11:14:49 +0100

    App providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,35 @@
 import "./assets/styles/App.css";
 import Home from "./pages/home.jsx";
 import MapPage from "./pages/mapPage.jsx";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.jsx";
 import Login from "./pages/login.jsx";
 import Profile from "./pages/profile.jsx";
 import ProtectedRoute from "./utils/protectedRoute.jsx";
 import { Routes, Route } from "react-router-dom";
-import { AppProvider } from "./context/AppContext";
 import { AuthProvider } from "./context/AuthContext.jsx";
-import { RouteProvider } from "./context/RouteContext";
+import { RouteProvider } from "./context/RouteContext.jsx";
 
 function App() {
   return (
     <>
-      <AppProvider>
+      <AuthProvider>
         <RouteProvider>
-          <AuthProvider>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route
-                path="/map"
-                element={
-                  <ProtectedRoute>
-                    <MapPage />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/login" element={<Login />} />
-              <Route path="/profile" element={<Profile />} />
-            </Routes>
-            <Navbar />
-          </AuthProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/map"
+              element={
+                <ProtectedRoute>
+                  <MapPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+          <Navbar />
         </RouteProvider>
-      </AppProvider>
+      </AuthProvider>
     </>
   );
 }
