4d8d795a93054678cf85c7c7f8248ca8c3db6d29
Naomi García Robles <naomigarciarobles21@gmail.com>
2025-01-29 22:24:37 +0100

    Change navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import { NavLink } from "react-router-dom";
 import homeIcon from "../assets/images/home.png";
-import mapIcon from "../assets/images/map2.png";
-import userIcon from "../assets/images/user.png";
+import homeIcon2 from "../assets/images/home2.png";
+import mapIcon2 from "../assets/images/maps2.png";
+import mapIcon1 from "../assets/images/maps1.png";
+import userIcon1 from "../assets/images/user1.png";
+import userIconActive from "../assets/images/user2.png";
 import { useAuth } from "../context/AuthContext";
 
 const Navbar = () => {
@@ -9,43 +12,34 @@ const Navbar = () => {
 
   return (
     <div className="fixed bottom-0 left-0 right-0 z-10 bg-white border-t p-3 flex justify-around items-center shadow-lg h-14">
-      <NavLink
-        to="/"
-        className={({ isActive }) =>
-          `rounded-full ${
-            isActive
-              ? "shadow-xl p-2 shadow-pastelBlue"
-              : "hover:shadow-xl hover:bg-pastelBlue"
-          }`
-        }
-      >
-        <img src={homeIcon} alt="Inicio" className="w-5 h-5" />
+      <NavLink to="/">
+        {({ isActive }) => (
+          <img
+            src={isActive ? homeIcon2 : homeIcon}
+            alt="Inicio"
+            className="w-6 h-6"
+          />
+        )}
       </NavLink>
 
-      <NavLink
-        to="/map"
-        className={({ isActive }) =>
-          ` rounded-full ${
-            isActive
-              ? "shadow-xl p-1 shadow-pastelBlue"
-              : "hover:shadow-xl hover:bg-pastelBlue"
-          }`
-        }
-      >
-        <img src={mapIcon} alt="Mapa" className="w-8 h-8" />
+      <NavLink to="/map">
+        {({ isActive }) => (
+          <img
+            src={isActive ? mapIcon2 : mapIcon1}
+            alt="Mapa"
+            className="w-6 h-6"
+          />
+        )}
       </NavLink>
 
-      <NavLink
-        to={user ? "/profile" : "/login"}
-        className={({ isActive }) =>
-          `rounded-full ${
-            isActive
-              ? "shadow-xl p-1 shadow-pastelBlue"
-              : "hover:shadow-xl hover:bg-pastelBlue"
-          }`
-        }
-      >
-        <img src={userIcon} alt="Mapa" className="w-6 h-6" />
+      <NavLink to={user ? "/profile" : "/login"}>
+        {({ isActive }) => (
+          <img
+            src={isActive ? userIconActive : userIcon1}
+            alt="Usuario"
+            className="w-6 h-6"
+          />
+        )}
       </NavLink>
     </div>
   );
