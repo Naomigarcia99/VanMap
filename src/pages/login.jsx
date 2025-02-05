f539e3778941e685ef300fa98cd4c397433ae9a8
Naomi García Robles <naomigarciarobles21@gmail.com>
2025-02-05 11:30:14 +0100

    Add password confirm input and visibility

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
+import Eye from "../assets/images/eye.png";
+import Eye2 from "../assets/images/eye2.png";
 
 const AuthPage = () => {
   const { registerUser, loginUser, loading, error, setError } = useAuth();
@@ -7,15 +9,27 @@ const AuthPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const togglePasswordShow = () => setShowPassword(!showPassword);
+  const toggleConfirmPasswordShow = () =>
+    setShowConfirmPassword(!showConfirmPassword);
 
   useEffect(() => {
     if (error) {
       setError(null);
     }
-  }, [email, name, password, isLogin]);
+  }, [email, name, password, isLogin, confirmPassword]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (!isLogin && password !== confirmPassword) {
+      setError("Las contraseñas no coinciden");
+      return;
+    }
 
     if (isLogin) {
       await loginUser({ email, password });
@@ -26,7 +40,7 @@ const AuthPage = () => {
 
   return (
     <section className="min-h-screen bg-gradient-to-r from-pastelBeige via-pastelBlue to-pastelGreen flex justify-center items-center p-4">
-      <div className="bg-white p-8 rounded-xl shadow-lg max-w-md">
+      <div className="bg-white p-8 rounded-xl shadow-lg max-w-md w-full">
         <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
           {isLogin ? "Iniciar sesión" : "Registrarse"}
         </h2>
@@ -52,13 +66,49 @@ const AuthPage = () => {
             onChange={(e) => setEmail(e.target.value)}
           />
 
-          <input
-            type="password"
-            placeholder="Contraseña"
-            className="w-full p-3 mb-6 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Contraseña"
+              className="w-full p-3 mb-6 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              onClick={togglePasswordShow}
+              className="absolute inset-y-0 right-4 bottom-5"
+            >
+              <img
+                src={showPassword ? Eye2 : Eye}
+                alt="Mostrar contraseña"
+                className="h-5 w-5"
+              />
+            </button>
+          </div>
+
+          {!isLogin && (
+            <div className="relative">
+              <input
+                type={showConfirmPassword ? "text" : "password"}
+                placeholder="Confirmar contraseña"
+                className="w-full p-3 mb-6 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <button
+                type="button"
+                onClick={toggleConfirmPasswordShow}
+                className="absolute inset-y-0 right-3 right-4 bottom-5"
+              >
+                <img
+                  src={showConfirmPassword ? Eye2 : Eye}
+                  alt="Mostrar contraseña"
+                  className="h-5 w-5"
+                />
+              </button>
+            </div>
+          )}
 
           <button
             type="submit"
